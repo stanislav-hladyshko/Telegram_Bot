beda6fbea44b1c9317523262246723466c028f76
stanislav-hladyshko <49280953+stanislav-hladyshko@users.noreply.github.com>
2019-08-30 18:54:57 +0300

    Updates

--- Parser/Parser/Program.cs
+++ Parser/Parser/Program.cs
@@ -16,27 +16,23 @@ namespace ConsoleApp33
         static void Main()
         {
             var url = "https://www.facebook.com/ulanasuprun/posts/2420760401541846";
+            var url2 = "https://www.facebook.com/ulanasuprun/posts/2423035161314370";
             HtmlWeb web = new HtmlWeb();
-            HtmlDocument loadHTML = web.Load(url);
+            HtmlDocument loadHTML = web.Load(url2);
             string parsingResult = "";
             string newLine = "\n";
-            
-            var parsingNode = loadHTML.DocumentNode.SelectSingleNode("//div[@class = 'hidden_elem']");
-            var temporaryStringResult = parsingNode.InnerHtml;
-
-            temporaryStringResult = temporaryStringResult.Replace("-->", string.Empty);
-            temporaryStringResult = temporaryStringResult.Replace("<!--", string.Empty);
-
-            loadHTML.LoadHtml(temporaryStringResult);
-
-            HtmlNode postMessageNode = loadHTML.DocumentNode.SelectSingleNode("//div[@data-testid='post_message']");
-            string resultContentOfHtmlParsing = "";
-            string temp = "\n";
-            foreach (HtmlNode node2 in postMessageNode.ChildNodes)
-            {
-                resultContentOfHtmlParsing += temp + node2.InnerText;
-            }
-            File.WriteAllText(@"c:\Users\Admin\Desktop\new_file.txt", resultContentOfHtmlParsing);
+            HtmlNodeCollection htmlNodes = loadHTML.DocumentNode.SelectNodes("//div[@class = 'hidden_elem']");
+            foreach (HtmlNode node in htmlNodes)
+                parsingResult += newLine + node.InnerHtml;
+            parsingResult = parsingResult.Replace("-->", string.Empty);
+            parsingResult = parsingResult.Replace("<!--", string.Empty);
+            parsingResult = parsingResult.Replace("<br />", "\n");
+            loadHTML.LoadHtml(parsingResult);
+            HtmlNode post_messageNode = loadHTML.DocumentNode.SelectSingleNode("//div[@data-testid='post_message']");
+            string resultOfHtmlParsing = "";
+            foreach (HtmlNode node2 in post_messageNode.ChildNodes)
+                resultOfHtmlParsing += newLine + node2.InnerText;
+            File.WriteAllText(@"c:\Users\Admin\Desktop\new_file.txt", resultOfHtmlParsing);
             string printTemp = File.ReadAllText(@"c:\Users\Admin\Desktop\new_file.txt");
             Console.Read();
         }
