4a9f1e005bdfe26418c85428995090ab3cf5a4f2
stanislav-hladyshko <49280953+stanislav-hladyshko@users.noreply.github.com>
2019-10-19 23:24:45 +0300

    Implementation hyperlink parsing possibility + TelegraPH parsing development

--- Parser/Parser/Program.cs
+++ Parser/Parser/Program.cs
@@ -1,6 +1,9 @@
 ﻿using HtmlAgilityPack;
+using Parser;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Text;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -14,15 +17,41 @@ namespace ConsoleApp33
             ParserMethod();
             Console.Read();
         }
+        static void TelegraphPostingMethod(HtmlNode contentArg)
+        {
+            var contentList = new List<TelegraphContent>();
+            foreach (var node in contentArg.ChildNodes)
+            {
+                contentList.Add(new TelegraphContent() { tag = new Tag { Name = "p" }, content = new Content() { ChildrenContent = node.InnerText } });
+            }
+
+            string resultContent = "[" + string.Join(",", contentList) + "]";
+
+            var client = new HttpClient();
+
+            var values = new Dictionary<string, string>
+            {
+                { "access_token", "35f1ca3764a3f666392c449bc9918457613949dfc2f252f12ab40c5924be" },
+                { "title", "Sample Page" },
+                { "author_name", "Anonymous" },
+                { "content", resultContent },
+                { "return_content", "true" }
+            };
+            var content = new FormUrlEncodedContent(values);
+            var response = client.PostAsync("https://api.telegra.ph/createPage", content);
+            var responseString = response.Result.Content.ReadAsStringAsync();
+
+        }
         static void ParserMethod()
         {
             Console.OutputEncoding = Encoding.UTF8;
-            var url = "https://www.facebook.com/ulanasuprun/posts/2458567217761164";
+            var url = "https://ru-ru.facebook.com/ulanasuprun/posts/2361155410835679";
             HtmlWeb web = new HtmlWeb();
             HtmlDocument loadedPage = web.Load(url);
             string parsingResult = "";
             string newLine = "\n";
             HtmlNodeCollection htmlNodes = loadedPage.DocumentNode.SelectNodes("//div[@class = 'hidden_elem']");
+            string a = loadedPage.DocumentNode.ChildNodes.ToString();
             foreach (HtmlNode node in htmlNodes)
                 parsingResult += newLine + node.InnerHtml;
             parsingResult = parsingResult.Replace("-->", string.Empty);
@@ -30,38 +59,84 @@ namespace ConsoleApp33
             parsingResult = parsingResult.Replace("<br />", "\n");
 
             loadedPage.LoadHtml(parsingResult);
-            ParseImage(loadedPage);
+           // ParseImage(loadedPage);
             HtmlNode post_messageNode = loadedPage.DocumentNode.SelectSingleNode("//div[@data-testid='post_message']");
-
-            urlAndNameConverter(post_messageNode.InnerHtml);
+            ConvertUrlsToTelegram(post_messageNode);
 
             string resultOfHtmlParsing = "";
             foreach (HtmlNode node2 in post_messageNode.ChildNodes)
                 resultOfHtmlParsing += node2.InnerText.TrimStart(' ') + newLine;
-
-            List<string> arr2 = TextDevidingOnParts(resultOfHtmlParsing);
-            foreach (string item in arr2)
-                Console.WriteLine(item);
+            List<TelegraphContent> arr2 = TextOnParts(resultOfHtmlParsing);
+            foreach (var item in arr2)
+            {
+                Console.WriteLine(item.ToString());
+            }
             Console.Read();
         }
-        static void urlAndNameConverter(string postHtmlString)
+        static void ConvertUrlsToTelegram(HtmlNode node)
         {
-            HtmlDocument aTagDocument = new HtmlDocument();
-            int firstIndex, lastIndex = 0;
-            while (true)
+            HtmlNode newNode = null;
+            foreach (var childNode in node.ChildNodes)
             {
-                if (postHtmlString.Contains("<a"))
+                var aTagNodes = childNode.SelectNodes("a");
+                foreach (var item in aTagNodes ?? Enumerable.Empty<HtmlNode>())
                 {
-                    firstIndex = postHtmlString.IndexOf("<a");
-                    lastIndex = postHtmlString.IndexOf("</a>") + 4;
+                    if (item.InnerText.StartsWith("http"))
+                    {
+                        newNode = HtmlNode.CreateNode(item.InnerText);
+                    }
+                    else if (item.InnerText.StartsWith("#"))
+                    {
+                        var title = item.InnerText;
+                        var url = $"https://www.facebook.com/hashtag/{item.InnerText.Substring(1)}";
+                        newNode = HtmlNode.CreateNode($"[{title}]({url})");
+                    }
+                    else
+                    {
+                        var title = item.InnerText;
+                        var url = item.GetAttributeValue("href", "google.com");
+                        newNode = HtmlNode.CreateNode($"[{title}]({url})");
+                    }
+                    childNode.ReplaceChild(newNode, item);
                 }
-                else
-                    break;
-                string aTag = postHtmlString.Substring(firstIndex, lastIndex - firstIndex);
-                aTagDocument.LoadHtml(aTag);
-                HtmlNode htmlNode = aTagDocument.DocumentNode.FirstChild;
-                string result = $"[{htmlNode.GetAttributeValue("title", "0")}]({htmlNode.GetAttributeValue("href", "0")})";
-                postHtmlString = postHtmlString.Remove(firstIndex, lastIndex - firstIndex);
+                if (childNode.HasChildNodes)
+                {
+                    ConvertUrlsToTelegram(childNode);
+                }
+            }
+        }
+
+        static void ConvertUrlsToTelegraph(HtmlNode node)
+        {
+            HtmlNode newNode = null;
+            foreach (var childNode in node.ChildNodes)
+            {
+                var aTagNodes = childNode.SelectNodes("a");
+                foreach (var item in aTagNodes ?? Enumerable.Empty<HtmlNode>())
+                {
+                    if (item.InnerText.StartsWith("http"))
+                    {
+                        newNode = HtmlNode.CreateNode(item.InnerText);
+                        //newNode = HtmlNode.CreateNode()
+                    }
+                    else if (item.InnerText.StartsWith("#"))
+                    {
+                        var title = item.InnerText;
+                        var url = $"https://www.facebook.com/hashtag/{item.InnerText.Substring(1)}";
+                        newNode = HtmlNode.CreateNode($"[{title}]({url})");
+                    }
+                    else
+                    {
+                        var title = item.InnerText;
+                        var url = item.GetAttributeValue("href", "google.com");
+                        newNode = HtmlNode.CreateNode($"[{title}]({url})");
+                    }
+                    childNode.ReplaceChild(newNode, item);
+                }
+                if (childNode.HasChildNodes)
+                {
+                    ConvertUrlsToTelegraph(childNode);
+                }
             }
         }
 
@@ -69,11 +144,12 @@ namespace ConsoleApp33
         {
             List<string> parsedImageUrlArray = new List<string>();
             HtmlNodeCollection htmlNodes = htmlDocument.DocumentNode.SelectNodes("//a/@data-ploi");
-            foreach (HtmlNode node in htmlNodes)
-            {
-                string url = node.GetAttributeValue("data-ploi", "666").Replace("amp;", string.Empty);
-                parsedImageUrlArray.Add(url);
-            }
+            if (htmlNodes != null)
+                foreach (HtmlNode node in htmlNodes)
+                {
+                    string url = node.GetAttributeValue("data-ploi", "666").Replace("amp;", string.Empty);
+                    parsedImageUrlArray.Add(url);
+                }
             return parsedImageUrlArray;
         }
         static List<string> TextDevidingOnParts(string text)
@@ -100,6 +176,30 @@ namespace ConsoleApp33
             }
             return resultList;
         }
-
+        static List<TelegraphContent> TextOnParts(string text)
+        {
+            int counter = 0;
+            char newLine = '\n';
+            string result = "";
+            string resultCount = "";
+            string[] arr = text.Split(newLine);
+            List<TelegraphContent> resultList = new List<TelegraphContent>();
+            while (resultCount.Length < text.Length)
+            {
+                if (result.Length < 4096 && counter < arr.Length)
+                {
+                    result += arr[counter] + newLine;
+                    counter++;
+                }
+                else
+                {
+                    resultList.Add(new TelegraphContent { tag = new Tag { Name = "p" }, content = new Content { ChildrenContent = result } });
+                    resultCount += result;
+                    result = "";
+                }
+                //return resultList;
+            }
+            return resultList;
+        }
     }
 }
---
+++ Parser/Parser/TelegraphContent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    internal class TelegraphContent
+    {
+        string taggg = "tag";
+        public Tag tag { get; set; }
+        public Content content { get; set; }
+        public override string ToString()
+        {
+            return ($"{{\"{tag.Tg}\":\"{tag.Name}\",\"{content.Children}\":[\"{content.ChildrenContent}\"]}}");
+        }
+    }
+    internal class Tag
+    {
+        public string Tg = "tag";
+        public string Name { get; set; }
+    }
+    internal class Content
+    {
+        public string Children = "children";
+        public string ChildrenContent { get; set; }
+    }
+}
