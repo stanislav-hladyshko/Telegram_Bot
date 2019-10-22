31dd14929bdfd1aecc8efd59bf01064ec72945e2
stanislav-hladyshko <49280953+stanislav-hladyshko@users.noreply.github.com>
2019-10-23 00:32:55 +0300

    Refactoring

--- Parser/Parser/Program.cs
+++ Parser/Parser/Program.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Text;
 using Telegram.Bot.Types;
 using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace ConsoleApp33
 {
@@ -14,9 +15,67 @@ namespace ConsoleApp33
     {
         static void Main()
         {
-            ParserMethod();
+            //ParserMethod();
+            RepostFacebookPost();
             Console.Read();
         }
+
+        static void RepostFacebookPost()
+        {
+            // Дістати з бази даних вже відправлені пости.
+            // ...
+            // Дістати список всіх постів з facebook.com/suprun/posts
+            // ...
+            // Зясувати чи є нові пости.
+            // ...
+            // Резутат знаходження нового посту це його id:
+            var newPostFacebookId = "2465964917021394";
+
+            // Отримуємо одну ноду з вмістом //div[@class = 'hidden_elem'] для парсинуг зображень.
+            // Отримуємо другу ноду з вмістом //div[@data-testid='post_message'] для парсинуг тексу.
+            var (htmlNodeForImagesParsing, htmlNodeForTextParsing) = GetHtmlNodesForImageAndText(newPostFacebookId);
+
+            List<string> images = ParseImage(htmlNodeForImagesParsing);
+
+            var telegramMarkdownNode = htmlNodeForTextParsing.Clone();
+            ConvertUrlsToTelegramMarkdown(telegramMarkdownNode);
+
+            string telegramPostContent = "";
+            foreach (HtmlNode node2 in telegramMarkdownNode.ChildNodes)
+                telegramPostContent += node2.InnerText.TrimStart(' ') + "\n";
+
+            if (telegramPostContent.Length < 4097)
+            {
+                TelegramPostingMethod(telegramPostContent, true);
+                TelegramPostingImagesMethod(images);
+                // Зберегти в базу id поста який відправили.
+                return;
+            }
+            string telegraphPostContent = telegraphParserMethodNEW(images, htmlNodeForTextParsing);
+            TelegraphPostingMethod(telegraphPostContent);
+            // Зберегти в базу id поста який відправили.
+        }
+
+        static (HtmlNode, HtmlNode) GetHtmlNodesForImageAndText(string id)
+        {
+            var url = "https://www.facebook.com/ulanasuprun/posts/" + id;
+            HtmlDocument loadedFacebookPage = new HtmlWeb().Load(url);
+
+            string parsingResult = "";
+            string newLine = "\n";
+            HtmlNodeCollection htmlNodes = loadedFacebookPage.DocumentNode.SelectNodes("//div[@class = 'hidden_elem']");
+            string a = loadedFacebookPage.DocumentNode.ChildNodes.ToString();
+            foreach (HtmlNode node in htmlNodes)
+                parsingResult += newLine + node.InnerHtml;
+            parsingResult = parsingResult.Replace("-->", string.Empty);
+            parsingResult = parsingResult.Replace("<!--", string.Empty);
+            parsingResult = parsingResult.Replace("<br />", "\n");
+            loadedFacebookPage.LoadHtml(parsingResult);
+            HtmlNode postMessageNode = loadedFacebookPage.DocumentNode.SelectSingleNode("//div[@data-testid='post_message']");
+
+            return (loadedFacebookPage.DocumentNode, postMessageNode);
+        }
+
         static void ParserMethod()
         {
             #region
@@ -37,30 +96,33 @@ namespace ConsoleApp33
             HtmlNode post_messageNode = loadedPage.DocumentNode.SelectSingleNode("//div[@data-testid='post_message']");
             #endregion
             string resultOfHtmlParsing = "";
-            ConvertUrlsToTelegram(post_messageNode);
+            //ConvertUrlsToTelegram(post_messageNode);
             foreach (HtmlNode node2 in post_messageNode.ChildNodes)
                 resultOfHtmlParsing += node2.InnerText.TrimStart(' ') + newLine;
 
-            List<string> imglist = ParseImage(loadedPage);
-
-            string ResultTelegraphContent = post_messageNode.InnerText;
-            TelegramPostingMethod(resultOfHtmlParsing);
-
+            List<string> imglist = ParseImage(loadedPage.DocumentNode);
             //List<TelegraphContent> arr2 = TextOnParts(resultOfHtmlParsing);
+            //string ResultTelegraphContent = post_messageNode.InnerText;
+
+            TelegramPostingMethod(resultOfHtmlParsing, false);
+            // ConvertUrlsToTelegram(post_messageNode);
+            telegraphParserMethod(loadedPage.DocumentNode, post_messageNode);
 
 
-            string telegraPHparsingResult = telegraphParserMethod(loadedPage, post_messageNode);
+
+            //string telegraPHparsingResult = telegraphParserMethod(loadedPage, post_messageNode);
+            //TelegraphPostingMethod(telegraPHparsingResult);
 
         }
 
-        static string telegraphParserMethod(HtmlDocument htmlDocument, HtmlNode htmlNode)
+        // TODO: Delete old method.
+        static string telegraphParserMethodNEW(List<string> imageUrls, HtmlNode htmlNode)
         {
             var listOfNodes = new List<Object>();
             foreach (var node in htmlNode.ChildNodes)
             {
                 listOfNodes.Add(domToNode(node));
             }
-            List<string> imageUrls = ParseImage(htmlDocument);// Парсимо зображення методом "ParseImage"
             foreach (var item in imageUrls)
             {
                 TelegraphNode imgNode = new TelegraphNode() { tag = "img" };
@@ -69,41 +131,64 @@ namespace ConsoleApp33
                 listOfNodes.Add(imgNode);
             }
             string output = JsonConvert.SerializeObject(listOfNodes, Formatting.None, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
-            return output;
+            return output;//в результаті маємо контент зі всіма тегами і посиланнями для постингу.
         }
 
-        static void TelegraphPostingMethod(HtmlNode telegraphContent)
+        // TODO: передавати одразу список посилань на зображення.
+        static string telegraphParserMethod(HtmlNode imagesNode, HtmlNode htmlNode)
         {
-            var contentList = new List<TelegraphContent>();
-            foreach (var node in telegraphContent.ChildNodes)
+            var listOfNodes = new List<Object>();
+            foreach (var node in htmlNode.ChildNodes)
             {
-                contentList.Add(new TelegraphContent() { tag = new Tag { Name = "p" }, content = new Content() { ChildrenContent = node.InnerText } });
+                listOfNodes.Add(domToNode(node));
             }
-            string resultContent = "[" + string.Join(",", contentList) + "]";
+            List<string> imageUrls = ParseImage(imagesNode);// Парсимо зображення методом "ParseImage"
+            foreach (var item in imageUrls)
+            {
+                TelegraphNode imgNode = new TelegraphNode() { tag = "img" };
+                imgNode.attrs = new Dictionary<string, string>();
+                imgNode.attrs["src"] = item;
+                listOfNodes.Add(imgNode);
+            }
+            string output = JsonConvert.SerializeObject(listOfNodes, Formatting.None, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+            return output;//в результаті маємо контент зі всіма тегами і посиланнями для постингу.
+        }
 
+        static void TelegraphPostingMethod(string telegraphContent)
+        {
             Dictionary<string, string> values = new Dictionary<string, string>
             {
-                { "access_token", "35f1ca3764a3f666392c449bc9918457613949dfc2f252f12ab40c5924be" },
-                { "title", "Sample Page" },
-                { "author_name", "Anonymous" },
-                { "content", resultContent },
+                { "title", "test Page" },
+                { "author_name", "Уляна Супрун" },
+                { "content", telegraphContent },
                 { "return_content", "true" }
             };
             var client = new HttpClient();
             var content = new FormUrlEncodedContent(values);
-            var response = client.PostAsync("https://api.telegra.ph/createPage", content);
-            var responseString = response.Result.Content.ReadAsStringAsync();
+            Task<HttpResponseMessage> task = client.PostAsync("https://api.telegra.ph/createPage?access_token=9596118885a2bafed8556475533dc09596d876fea8b39ed6d191d05d9b67&", content);
+            var responseString = task.Result.Content.ReadAsStringAsync().Result;
+
+            if (task.Result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                TelegraphResponse thResponse = JsonConvert.DeserializeObject<TelegraphResponse>(responseString);
+                TelegramPostingMethod(thResponse.result.url, true);
+            }
         }
-        static void TelegramPostingMethod(string telegramContent)
+
+        static void TelegramPostingMethod(string telegramContent, bool isTelegramPost)
         {
-            Console.WriteLine();
+            telegramContent = telegramContent.Replace('`', '\'');
             Dictionary<string, string> values = new Dictionary<string, string>
             {
                 { "chat_id", "@stanislav_hladyshko"},
-             { "parse_mode", "markdown"},
-                { "text", telegramContent },
-                { "disable_web_page_preview", "true"}
+                { "parse_mode", "markdown"},
+                { "text", telegramContent }
             };
+            if (isTelegramPost)
+            {
+                values["disable_web_page_preview"] = "false";
+            }
+            
             var httpClient = new HttpClient();
             HttpContent content = new FormUrlEncodedContent(values);
             string telegramToken = "https://api.telegram.org/bot980261769:AAGPe8mb1Cuq4wWPu-JBdFnSC_nr9aW9--k/sendMessage";
@@ -111,7 +196,11 @@ namespace ConsoleApp33
 
         }
 
-        static void ConvertUrlsToTelegram(HtmlNode node)
+        static void TelegramPostingImagesMethod(List<string> imageList)
+        {
+
+        }
+        static void ConvertUrlsToTelegramMarkdown(HtmlNode node)
         {
             HtmlNode newNode = null;
             foreach (var childNode in node.ChildNodes)
@@ -139,14 +228,14 @@ namespace ConsoleApp33
                 }
                 if (childNode.HasChildNodes)
                 {
-                    ConvertUrlsToTelegram(childNode);
+                    ConvertUrlsToTelegramMarkdown(childNode);
                 }
             }
         }
-        static List<string> ParseImage(HtmlDocument htmlDocument)
+        static List<string> ParseImage(HtmlNode htmlNode)
         {
             List<string> parsedImageUrlArray = new List<string>();
-            HtmlNodeCollection htmlNodes = htmlDocument.DocumentNode.SelectNodes("//a/@data-ploi");
+            HtmlNodeCollection htmlNodes = htmlNode.SelectNodes("//a/@data-ploi");
             if (htmlNodes != null)
                 foreach (HtmlNode node in htmlNodes)
                 {
@@ -155,55 +244,55 @@ namespace ConsoleApp33
                 }
             return parsedImageUrlArray;
         }
-        static List<string> TextDevidingOnParts(string text)
-        {
-            int counter = 0;
-            char newLine = '\n';
-            string result = "";
-            string resultCount = "";
-            string[] arr = text.Split(newLine);
-            List<string> resultList = new List<string>();
-            while (resultCount.Length < text.Length)
-            {
-                if (result.Length < 4096 && counter < arr.Length)
-                {
-                    result += arr[counter] + newLine;
-                    counter++;
-                }
-                else
-                {
-                    resultList.Add(result.TrimStart(' '));
-                    resultCount += result;
-                    result = "";
-                }
-            }
-            return resultList;
-        }
-        static List<TelegraphContent> TextOnParts(string text)
-        {
-            int counter = 0;
-            char newLine = '\n';
-            string result = "";
-            string resultCount = "";
-            string[] arr = text.Split(newLine);
-            List<TelegraphContent> resultList = new List<TelegraphContent>();
-            while (resultCount.Length < text.Length)
-            {
-                if (result.Length < 4096 && counter < arr.Length)
-                {
-                    result += arr[counter] + newLine;
-                    counter++;
-                }
-                else
-                {
-                    resultList.Add(new TelegraphContent { tag = new Tag { Name = "p" }, content = new Content { ChildrenContent = result } });
-                    resultCount += result;
-                    result = "";
-                }
-                //return resultList;
-            }
-            return resultList;
-        }
+        //static List<string> TextDevidingOnParts(string text)
+        //{
+        //    int counter = 0;
+        //    char newLine = '\n';
+        //    string result = "";
+        //    string resultCount = "";
+        //    string[] arr = text.Split(newLine);
+        //    List<string> resultList = new List<string>();
+        //    while (resultCount.Length < text.Length)
+        //    {
+        //        if (result.Length < 4096 && counter < arr.Length)
+        //        {
+        //            result += arr[counter] + newLine;
+        //            counter++;
+        //        }
+        //        else
+        //        {
+        //            resultList.Add(result.TrimStart(' '));
+        //            resultCount += result;
+        //            result = "";
+        //        }
+        //    }
+        //    return resultList;
+        //}
+        //static List<TelegraphContent> TextOnParts(string text)
+        //{
+        //    int counter = 0;
+        //    char newLine = '\n';
+        //    string result = "";
+        //    string resultCount = "";
+        //    string[] arr = text.Split(newLine);
+        //    List<TelegraphContent> resultList = new List<TelegraphContent>();
+        //    while (resultCount.Length < text.Length)
+        //    {
+        //        if (result.Length < 4096 && counter < arr.Length)
+        //        {
+        //            result += arr[counter] + newLine;
+        //            counter++;
+        //        }
+        //        else
+        //        {
+        //            resultList.Add(new TelegraphContent { tag = new Tag { Name = "p" }, content = new Content { ChildrenContent = result } });
+        //            resultCount += result;
+        //            result = "";
+        //        }
+        //        //return resultList;
+        //    }
+        //    return resultList;
+        //}
         static object domToNode(HtmlNode node)
         {
             if (node.NodeType == HtmlNodeType.Text)
---
+++ Parser/Parser/TelegraphResponse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    public class Attrs
+    {
+        public string src { get; set; }
+    }
+
+    public class TelegraphJsonContent
+    {
+        public string tag { get; set; }
+        public IList<object> children { get; set; }
+        public Attrs attrs { get; set; }
+    }
+
+    public class Result
+    {
+        public string path { get; set; }
+        public string url { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string author_name { get; set; }
+        public IList<TelegraphJsonContent> content { get; set; }
+        public int views { get; set; }
+        public bool can_edit { get; set; }
+    }
+
+    public class TelegraphResponse
+    {
+        public bool ok { get; set; }
+        public Result result { get; set; }
+    }
+}
